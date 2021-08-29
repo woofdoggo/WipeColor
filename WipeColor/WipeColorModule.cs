88d8f6a14d3a186fb2afddfff28bd062108f64cb
WoofWoofDoggo <46545045+WoofWoofDoggo@users.noreply.github.com>
2021-08-28 21:52:30 -0400

    Split IL hooks into separate file (2)

--- WipeColor/WipeColorModule.cs
+++ WipeColor/WipeColorModule.cs
@@ -61,46 +61,5 @@ namespace Celeste.Mod.WipeColor {
                 Monocle.Engine.ClearColor = Color.Black;
             }
         }
-
-        private static Color GetWipeColor() {
-            if (Settings.ModEnabled) return WipeColor;
-            else return Color.Black;
-        }
-
-        private static void FadeRenderHook(ILContext context) {
-            ILCursor cursor = new ILCursor(context);
-
-            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdsfld("Celeste.ScreenWipe", "WipeColor"))) {
-                Logger.Log("WipeColor/FadeRender", $"Applying wipe color hook to {cursor.Index} in {cursor.Method.FullName}");
-
-                cursor.Index--;
-                cursor.Remove();
-                cursor.EmitDelegate<Func<Color>>(GetWipeColor);
-            }
-        }
-
-        private static void HeartRenderHook(ILContext context) {
-            ILCursor cursor = new ILCursor(context);
-
-            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdsfld("Celeste.ScreenWipe", "WipeColor"))) {
-                Logger.Log("WipeColor/HeartRender", $"Applying wipe color hook to {cursor.Index} in {cursor.Method.FullName}");
-
-                cursor.Index--;
-                cursor.Remove();
-                cursor.EmitDelegate<Func<Color>>(GetWipeColor);
-            }
-        }
-
-        private static void SpotlightRenderHook(ILContext context) {
-            ILCursor cursor = new ILCursor(context);
-            
-            while (cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdsfld("Celeste.ScreenWipe", "WipeColor"))) {
-                Logger.Log("WipeColor/SpotlightRender", $"Applying wipe color hook to {cursor.Index} in {cursor.Method.FullName}");
-
-                cursor.Index--;
-                cursor.Remove();
-                cursor.EmitDelegate<Func<Color>>(GetWipeColor);
-            }
-        }
     }
 }
